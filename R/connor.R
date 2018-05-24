3850fc30a1e7e9ef8fb9b67f4327ff4e0308639e
Taavi Päll <taavi74@ut.ee>
2018-05-24 17:49:14 +0300

    first version

--- R/connor.R
+++ R/connor.R
@@ -3,24 +3,55 @@
 #'ehk selles artiklis - viiruse väljumine ravi kontrolli alt peale seda kui ta on korra kontrolli alla saadud
 
 library(tidyverse)
+library(grid)
 library(gridExtra)
+library(gtable)
 library(viridis)
 
 (rb <- read_csv2("data/rebound_sample_vol1.csv"))
-rb <- mutate(rb, year = parse_number(year))
+rb <- mutate(rb, time = parse_number(year))
 
 
 rb_gathered <- filter(rb, !is.na(time)) %>% 
   select(time, `rep per y`, `inter per y`) %>% 
   gather(key, value, -time)
 
-
-ggplot(data = rb_gathered, mapping = aes(x = time, y = value, color = key)) +
+#' plotgrob
+p <- ggplot(data = rb_gathered, mapping = aes(x = time, y = value, color = key)) +
   geom_point() +
   geom_line() +
-  labs(x = "Years") +
+  labs(x = "Years from baseline") +
+  scale_x_continuous(breaks = unique(rb_gathered$time)) +
   theme_minimal() +
-  theme(legend.position = c(0.8, 0.8),
+  theme(axis.title.y = element_blank(),
+        legend.background = element_blank(),
+        legend.key = element_rect(colour = 0, inherit.blank = TRUE),
+        legend.position = c(0.8, 0.8),
         legend.title = element_blank())
+pg <- ggplotGrob(p)
 
+#' table grob
+rb_sideways <- select(rb, `viral rebound`, `person years`) %>% 
+  as.matrix() %>% t() %>% 
+  as_data_frame() 
+colnames(rb_sideways) <- rb$year
+rb_sw <- bind_cols(data_frame(at_risk = c("viral rebound", "person years")), rb_sideways)
+tbl <- tableGrob(rb_sw, cols = NULL, rows = NULL, theme = ttheme_minimal(padding = unit(c(8, 4), "mm")))
+tbl$widths <- unit(rep(1/ncol(rb_sw), ncol(rb_sw)), "npc")
 
+#' other bits and pices
+rect <- rectGrob(gp = gpar(fill = 0, col = 0))
+y.title <- grobTree(rect, textGrob("Viral rebound?", rot = 90, vjust = 2))
+firstcol.title <- grobTree(rect, textGrob("Number at risk", vjust = 1))
+lastcol.title <- grobTree(rect, textGrob("Other", vjust = 1))
+mat <- matrix(list(y.title, pg, rect, firstcol.title, rect, lastcol.title), nrow = 2, byrow = TRUE)
+
+g <- gtable_matrix(name = "plot", grobs = mat, 
+                   widths = unit(c(1/ncol(rb_sw), (ncol(rb_sw) - 2)/ncol(rb_sw), 1/ncol(rb_sw)), "npc"), 
+                   heights = unit(c(9/10, 1/10), c("npc")))
+
+gg <- gtable_matrix(name = "tab", grobs = matrix(list(g, tbl), nrow = 2), 
+                    widths = unit(1, "npc"),
+                    heights = unit(c(9/10, 1/10), "npc"))
+
+grid.draw(gg)
